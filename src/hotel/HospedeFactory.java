22d0abe5f3e913810a1d6cbb1373db5da5f7ec11
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-15 11:42:57 -0300

    Add files via upload

---
+++ src/hotel/HospedeFactory.java
@@ -0,0 +1,20 @@
+package hotel;
+
+import excecoes.Excecoes;
+
+public class HospedeFactory {
+	private String nomeHospede;
+	private String emailHospede;
+	private int anoNascimento;
+	
+public HospedeFactory(String nomeHospede, String emailHospede, int anoNascimento){
+		
+		this.nomeHospede = nomeHospede;
+		this.emailHospede = emailHospede;
+		this.anoNascimento = anoNascimento;
+	}
+
+	public Hospede criaHospede(String nomeHospede, String emailHospede, String anoNascimento) throws Exception{
+		return new Hospede(nomeHospede, emailHospede, anoNascimento);
+	}
+}
