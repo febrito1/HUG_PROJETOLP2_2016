30e3878c14dbf18ed9f2dac00b4788df95907379
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-16 15:15:25 -0300

    Add files via upload

--- src/hotel/SistemaController.java
+++ src/hotel/SistemaController.java
@@ -1,89 +1,56 @@
 package hotel;
 
-import java.util.HashSet;
-import excecoes.*;
-import hotel.*;
+import java.util.HashMap;
+
+import excecoes.Excecoes;
 
 public class SistemaController {
 	
-	private HashSet<Hospede> clientesCadastrados;
-	private HashSet<Hospede> clientesHospedados;
 	
+	private FactoryDeHospede factoryHospedes;
+	private HashMap<String, Hospede> clientesCadastrados;
+	private HashMap<String, Hospede> clientesHospedados;
+
 	public SistemaController() {
-		
-		clientesCadastrados = new HashSet<Hospede>();
+
+		clientesCadastrados = new HashMap<String, Hospede>();
 
 	}
+	public String cadastraHospede(String nome, String email, String dataNascimento) throws Exception{
+		clientesCadastrados.put(email, factoryHospedes.criaHospede(nome, email, dataNascimento));
+		return email;
+	}
+	
+	public String buscaHospede(String email) throws Exception {
+		if (!clientesCadastrados.containsKey(email)) {
+			throw new Exception("Erro na consulta de hospede. Hospede de email " + email + " nao foi cadastrado(a).");
+		}
+		return email;
+	}
+		
+	
+	public void removeHospede(String email) throws Exception {
+			if (!clientesCadastrados.containsKey(email)) {
+				throw new Exception("Erro na consulta de hospede. Hospede de email " + email + " nao foi cadastrado(a).");
+			}
+			clientesCadastrados.remove(email);
+		}
 
-	public boolean cadastraSistema(String nome, String email, int anoNascimento) throws Exception{
-		Excecoes.StringException(nome);
-		Excecoes.StringException(email);
-		Excecoes.inteiroException(anoNascimento);
-		
-		return clientesCadastrados.add(new Hospede(nome, email, anoNascimento));
-	}
-	
-	public boolean mudaNome(String email, String nome) throws Exception{
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(email)){
-				hospede.setNomeHospede(nome);
-				return true;
-			} 
-		}return false;
-		
-	}
-	
-	public void mudaEmail(String emailAntigo, String email) throws Exception{
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(emailAntigo)){
-				hospede.setEmailHospede(emailAntigo);
-			}
+	public void atualizaCadastro(String id, String atributo, String valor) throws Exception {
+		if (!clientesCadastrados.containsKey(id)) {
+			throw new Exception("Erro na consulta de hospede. Hospede de email " + id + " nao foi cadastrado(a).");
 		}
+		Hospede clienteatualizado = clientesCadastrados.get(id); 
+		switch (atributo.toLowerCase()){
+		case("nome"):
+			clienteatualizado.setNomeHospede(valor);
+		case("email"):
+			clienteatualizado.setEmailHospede(valor);
+			clientesCadastrados.remove(id);
+			clientesCadastrados.put(valor, clienteatualizado);
+		case("ano"):
+			clienteatualizado.setAnoNascimento(valor);
+			break;
+		} 
 	}
-	
-	public void mudaDataNascimento(String email, int anoNascimento)throws Exception{
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(email)){
-				hospede.setAnoNascimento(anoNascimento);
-			}
-		}
-	}
-	
-	public String buscaHospede(String email) throws Exception{
-		Excecoes.StringException(email);
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(email)){
-				return hospede.getEmailHospede();
-			}
-		} return "Erro na consulta de hospede. Hospede de email " + email + "nao foi cadastrado(a).";
-	}
-	
-	public Hospede getHospete(String email) throws Exception{
-		Hospede procuraHospede = null;
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(email)){
-				procuraHospede = hospede;
-			}
-		} throw new Exception("Hospede nao encontrado.");
-	}
-	
-	
-	public boolean removeHospede(String email){
-		for (Hospede hospede : clientesCadastrados) {
-			if(hospede.getEmailHospede().equalsIgnoreCase(email)){
-				return clientesCadastrados.remove(hospede);
-			}
-		} return false;
-	}
-	
-	/* RESOLVER QUESTOES DE HOSPEDE/ESTADIA*/
-	
-	/*public boolean checkIn(Hospede hospede) throws Exception{
-		
-	}*/
-	
-	public String checkIn(String email, String quarto, int dias) throws Exception{
-		for
-	}
-		
 }
