60b65e25b01cb340dd4064bd34cc14cf131f70fe
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-17 17:01:28 -0300

    Add files via upload

--- src/testes/HospedeTest.java
+++ src/testes/HospedeTest.java
@@ -9,69 +9,68 @@ import hotel.Hospede;
 import junit.framework.Assert;
 import quarto.Quarto;
 
-class HospedeTest {
+public class HospedeTest {
 
 	@Before
 	public void setUp() throws Exception {
-		Hospede novoHospede = new Hospede("Livia", "livia_123@em.com", "1990");
-		assertEquals("Livia", novoHospede.getNomeHospede());
-		assertEquals("livia_123@em.com", novoHospede.getEmailHospede());
-		assertEquals(1990, novoHospede.getAnoNascimento());
+		Hospede novoHospede = new Hospede("Livia", "livia_123@em.com", "29/04/1996");
+		Assert.assertEquals("Livia", novoHospede.getNomeHospede());
+		Assert.assertEquals("livia_123@em.com", novoHospede.getEmailHospede());
+		Assert.assertEquals("29/04/1996", novoHospede.getAnoNascimento());
 
 	}
 
 	@Test
 	public void testeSets() throws Exception {
-		Hospede novoHospede = new Hospede("Livia", "livia_123@em.com", "1990");
+		Hospede novoHospede = new Hospede("Livia", "livia_123@em.com", "29/04/1996");
 		novoHospede.setNomeHospede("Rebeca");
 		novoHospede.setEmailHospede("rebeca_beltrao@em.com");
-		novoHospede.setAnoNascimento("1996");
-		assertEquals("Rebeca", novoHospede.getNomeHospede());
-		assertEquals("rebeca_beltrao@em.com", novoHospede.getEmailHospede());
-		assertEquals("1996", novoHospede.getAnoNascimento());
+		novoHospede.setAnoNascimento("29/04/1996");
+		Assert.assertEquals("Rebeca", novoHospede.getNomeHospede());
+		Assert.assertEquals("rebeca_beltrao@em.com", novoHospede.getEmailHospede());
+		Assert.assertEquals("29/04/1996", novoHospede.getAnoNascimento());
 
 	}
 
 	@Test
 	public void testeComException() throws Exception {
 		try {
-			new Hospede(null, "rebeca_beltrao@em.com", "1996");
+			new Hospede(null, "rebeca_beltrao@em.com", "29/04/1996");
+		} catch (Exception e) {
+			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
+		}
+		
+		try {
+			new Hospede("", "rebeca_beltrao@em.com","29/04/1996");
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
 		try {
-			new Hospede("Rebeca", "rebeca_beltrao@em.com", "-12");
-		} catch (Exception e) {
-			assertEquals("Mensagem de excecao capturada.", "Numero nao pode ser negativo ou zero.", e.getMessage());
-		}
-		try {
-			new Hospede("", "rebeca_beltrao@em.com", "1996");
+			new Hospede("Rebeca", null,"29/04/1996");
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
 		try {
-			new Hospede("Rebeca", "rebeca_beltrao@em.com", "0");
-		} catch (Exception e) {
-			assertEquals("Mensagem de excecao capturada.", "Numero nao pode ser negativo ou zero.", e.getMessage());
-		}
-		try {
-			new Hospede("Rebeca", null, "1996");
+			new Hospede("Rebeca","", "29/04/1996");
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
 		try {
-			new Hospede("Rebeca","", "1996");
+			new Hospede(null, null, "29/04/1996");
+		} catch (Exception e) {
+			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
+		}try {
+			new Hospede("Rebeca", "rebeca_beltrat@em.com", "");
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
 		try {
-			new Hospede(null, null, "1996");
+			new Hospede(null, null, null);
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
-
 		try {
-			new Hospede(null, "", "1996");
+			new Hospede(null, "", "29/04/1996");
 		} catch (Exception e) {
 			assertEquals("Mensagem de excecao capturada.", "String nao pode ser nula ou vazia", e.getMessage());
 		}
