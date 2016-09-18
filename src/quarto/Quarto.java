1b32b759860f98308074610038602225db8da316
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-18 17:32:22 -0300

    Quarto com equals

--- src/quarto/Quarto.java
+++ src/quarto/Quarto.java
@@ -1,35 +1,43 @@
 package quarto;
 
-
+import hotel.Estadia;
 
 public class Quarto{
-	
+
 	private String ID;
-	private boolean estadoQuarto = false;
 	private TipoQuarto tipo;
 
 	public Quarto(String ID, TipoQuarto tipo) throws Exception {
 		this.ID = ID;
 		this.tipo = tipo;
 	}
-	
-	public String getID(){
+
+	public String getID() {
 		return this.ID;
 	}
-	
-	public double getPreco(){
-		return tipo.getPreco();
+
+	public double getPreco() {
+		return this.getPreco();
 	}
 
-	public boolean isEstadoQuarto() {
-		return estadoQuarto;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
 	}
 
-	public void setEstadoQuarto(boolean estadoQuarto) {
-		if(estadoQuarto = true){
-			estadoQuarto = false;
-		}else{
-			estadoQuarto = true;
-		}	
+	@Override
+	public boolean equals(Object outroQuarto) {
+		if(outroQuarto instanceof Quarto){
+			Quarto novo = (Quarto) outroQuarto;
+			if(novo.getID().equalsIgnoreCase(ID)){
+				return true;
+			}
+		}
+		return false;
 	}
+
 }
