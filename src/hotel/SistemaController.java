304e4f9921583631989692265837d550e57abadf
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-19 17:34:09 -0300

    mudanças idade

--- src/hotel/SistemaController.java
+++ src/hotel/SistemaController.java
@@ -1,39 +1,38 @@
 package hotel;
 
-
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import Exceptions.ConsultaHospedagemException;
-import excecoes.Excecoes;
 import quarto.Quarto;
 import quarto.QuartosFactory;
 
 public class SistemaController {
 	
-	
-	FactoryDeHospede factoryHospedes;
-	QuartosFactory factoryQuartos;
-	
-	Map<String, Hospede> clientesCadastrados;
-	Map<String, Quarto> catalogoQuartos;
-	List<Estadia> estadias;
+	private FactoryDeHospede factoryHospedes;
+	private Map<String, Hospede> clientesCadastrados;
+	private Map<String, Quarto> catalogoQuartos;
+	private Hospede hospede;
+	LocalDate dataNascimento;
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	public SistemaController() {
 
-		catalogoQuartos = new HashMap<>();
 		clientesCadastrados = new HashMap<String, Hospede>();
 		factoryHospedes = new FactoryDeHospede();
-		factoryQuartos = new QuartosFactory();
-		estadias = new ArrayList<>();
 
 	}
 	
-	public void iniciaSistema() {}
-	
 	public String cadastraHospede(String nome, String email, String dataNascimento) throws Exception{
+		
+		String novoAnoNascimento = formatter.format(this.dataNascimento);
+		LocalDate data = LocalDate.parse(novoAnoNascimento, formatter);
+		int idade = (int)ChronoUnit.YEARS.between(data, LocalDate.now());
+		if(!(idade >= 18)){
+			throw new Exception("Erro no cadastro de Hospede. A idade do(a) hospede deve ser maior que 18 anos.");
+		}
 		clientesCadastrados.put(email, factoryHospedes.criaHospede(nome, email, dataNascimento));
 		return email;
 	}
@@ -61,25 +60,21 @@ public class SistemaController {
 		switch (atributo.toLowerCase()){
 		case("nome"):
 			clienteatualizado.setNomeHospede(valor);
-			break;
 		case("email"):
 			clienteatualizado.setEmailHospede(valor);
 			clientesCadastrados.remove(id);
 			clientesCadastrados.put(valor, clienteatualizado);
-			break;
-		case("data de nascimento"):
+		case("ano"):
 			clienteatualizado.setAnoNascimento(valor);
 			break;
 		} 
 		
 	}
 	
-	public String getInfo(String Info, String atributo) throws Exception {
-		
+	public String getInfo(String Info, String atributo) throws Exception{
 		if(!(clientesCadastrados.containsKey(Info))){
 			throw new Exception("Erro na consulta de hospede. Hospede de email " + Info + " nao foi cadastrado(a).");
 		}
-		
 		String informacao = "";
 		Hospede hospedeInfo = clientesCadastrados.get(Info);
 		
@@ -98,112 +93,15 @@ public class SistemaController {
 	}
 	
 
-	public String criaQuarto(String ID, String tipoQuarto) throws Exception {
+	public String criaQuarto(String ID, String tipoQuarto) throws Exception{
 		if(catalogoQuartos.containsKey(ID)){
 			throw new Exception("O quarto de ID" + ID + " já existe.");
 		}
-		factoryQuartos.criaQuarto(ID, tipoQuarto);
+		QuartosFactory novoQuarto = new QuartosFactory();
+		novoQuarto.criaQuarto(ID, tipoQuarto);
+		
 		return ID;
 	}
 	
-	
-	public void checkIn(String email, int dias, String ID, String tipoQuarto) throws Exception {
-		/* tratar excecoes de email, ID nulos ou vazios */
-		Excecoes.tipoInvalido(tipoQuarto);
-		
-		if(!(clientesCadastrados.containsKey(email))){
-			throw new Exception("Erro na consulta de hospede. Hospede de email " + ID + " nao foi cadastrado(a).");
-		}
-	
-		Hospede cliente = clientesCadastrados.get(email);
-		
-		if((catalogoQuartos.containsKey(ID) && catalogoQuartos.get(ID).getTipo().equalsIgnoreCase(tipoQuarto))){
-			Quarto quarto = catalogoQuartos.get(ID);
-			
-			for (Estadia estadia : estadias) {
-				if(estadia.getQuarto().equals(quarto)){
-					throw new Exception("Erro ao realizar checkin. Quarto " + ID + " ja esta ocupado.");
-				}
-			}
-		
-			Estadia novaEstadia = new Estadia(cliente, quarto, dias);
-			estadias.add(novaEstadia);
-		}else{
-			Quarto quarto = factoryQuartos.criaQuarto(ID, tipoQuarto);
-			catalogoQuartos.put(ID, quarto);
-			Estadia estadia = new Estadia(cliente, quarto , dias);
-			estadias.add(estadia);
-		}
-		
-	}
-
-	public String getInfoHospedagem(String email, String atributo) throws Exception {
-		
-		if(!clientesCadastrados.containsKey(email)){
-			throw new Exception("Erro no cadastro de Hospede. Hospede nao cadastrado");
-		}
-		
-		Hospede hospede = clientesCadastrados.get(email);
-		String resultado = "";
-		
-		switch(atributo.toLowerCase()){
-		
-		case "hospedagens ativas":
-			int estadiasAtivas = 0;	
-			for (Estadia estadia : estadias) {
-				if(estadia.getHospede().equals(hospede)){
-					estadiasAtivas +=1;
-				}
-			}	
-			if(estadiasAtivas == 0){
-				throw new Exception("Erro na consluta de hospede "+ hospede.getNomeHospede()+
-						" nao esta hospedado(a).");
-			}
-			resultado = Integer.toString(estadiasAtivas);
-			break;
-		
-		case "total":
-			double dinheiroTotal = 0.0;
-			
-			for (Estadia estadia : estadias) {
-				if(estadia.getHospede().equals(hospede)) {
-					dinheiroTotal = estadia.getTotal();
-				}
-			}
-		
-			if(dinheiroTotal == 0.0){
-				throw new ConsultaHospedagemException("Hospede " + hospede.getNomeHospede() +
-						" nao esta hospedado(a).");
-			}
-		
-			resultado = String.format("R$%.2f", dinheiroTotal);
-			break;
-			
-		case "quarto":	
-			for (Estadia estadia : estadias) {
-				if(estadia.getHospede().equals(hospede));
-				Quarto quarto = estadia.getQuarto();
-				resultado += quarto.getID() + ",";
-			}
-			
-			if(resultado.isEmpty()){
-				throw new ConsultaHospedagemException("Hospede " + hospede.getNomeHospede()+
-						" nao esta hospedado(a).");
-				
-			} else if(resultado.charAt(resultado.length() -1)==','){
-					resultado = resultado.substring(0, resultado.length()-1);
-			}
-			break;
-			
-		default:
-			throw new ConsultaHospedagemException("Atributo de pesquisa invalido.");
-			
-		}
-		return resultado;		
-	}
-	
-	
-	
-	public void fechaSistema(){}
 
 }
