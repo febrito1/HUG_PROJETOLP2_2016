d0700dca57a440ee9b082f0b0b04139d8cfad299
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-19 17:44:56 -0300

    Add files via upload

---
+++ src/excecoes/ConsultaHospedagemException.java
@@ -0,0 +1,9 @@
+package excecoes;
+
+public class ConsultaHospedagemException extends Exception{
+	
+	public ConsultaHospedagemException(String mensagem){
+		super("Erro na consulta de hospedagem. "+mensagem);
+	}
+
+}
