3a751f58165f2f5ddf91b79dcee236584f55d312
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-20 15:25:41 -0300

    Mudancas em pratos factory

--- src/restaurante/PratosFactory.java
+++ src/restaurante/PratosFactory.java
@@ -1,25 +1,15 @@
-package restaurante;
 
-public class PratosFactory{
-	
-	public Prato criaPrato(String nomePrato, double precoPrato, String descricaoPrato, String tipoPrato) throws Exception{
-		
-		Prato novoPrato = null;
-		
-		switch(tipoPrato.toLowerCase()){
-		case "entrada":
-			novoPrato = new Prato(nomePrato, precoPrato, descricaoPrato, TipoPrato.ENTRADA);
-			break;
-		case "principal":
-			novoPrato = new Prato(nomePrato, precoPrato, descricaoPrato, TipoPrato.PRINCIPAL);
-			break;
-		case "sobremesa":
-			novoPrato = new Prato(nomePrato, precoPrato, descricaoPrato, TipoPrato.SOBREMESA);
-			break;
-		case "petit":
-			novoPrato = new Prato(nomePrato, precoPrato, descricaoPrato, TipoPrato.PETIT);
-			break;
+package restaurante;
+	 
+	 
+	import excecoes.Excecoes;
+	 
+	 public class PratosFactory {
+	 
+	 	public PratosFactory(){}
+	 	
+	 	public Prato criaPrato(String nomePrato, double precoPrato, String descricaoPrato) throws Exception{
+			 return new Prato(nomePrato, precoPrato, descricaoPrato);
 		}
-		return novoPrato;
-	}
-}
+	 	
+	 }
