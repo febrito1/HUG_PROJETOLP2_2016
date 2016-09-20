96ed3d8c7b47d16efe3439d675408e19b207b713
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-20 15:27:37 -0300

    Add files via upload

--- src/hotel/Facade.java
+++ src/hotel/Facade.java
@@ -1,13 +1,19 @@
-package hotel;
+package sistema;
 
 import easyaccept.EasyAccept;
+import excecoes.Excecoes;
+import restaurante.Prato;
+import restaurante.RefeicaoCompleta;
+import restaurante.RestauranteController;
 
 public class Facade {
-	
+
 	private SistemaController controler;
-	
+	private RestauranteController controlerRestaurante;
+
 	public Facade() {
 		controler = new SistemaController();
+
 	}
 
 	public void iniciaSistema() {
@@ -15,53 +21,75 @@ public class Facade {
 	}
 
 	public String cadastraHospede(String nome, String email, String dataNascimento) throws Exception {
-		
 		return controler.cadastraHospede(nome, email, dataNascimento);
 	}
 
 	public String getInfoHospede(String id, String atributo) throws Exception {
-		return controler.getInfoHospede(id, atributo);
+		return controler.getInfo(id, atributo);
 	}
 
 	public void atualizaCadastro(String id, String atributo, String valor) throws Exception {
-		
 		controler.atualizaCadastro(id, atributo, valor);
-		
+
 	}
 
 	public void removeHospede(String email) throws Exception {
 		controler.removeHospede(email);
 	}
-	
-	public void realizaCheckin(String email, int dias, String idQuarto, String tipoQuarto) throws Exception{
+
+	public void realizaCheckin(String email, int dias, String idQuarto, String tipoQuarto) throws Exception {
 		controler.realizaCheckin(email, dias, idQuarto, tipoQuarto);
 	}
 
-	
-	public String getInfoHospedagem(String email, String atributo) throws Exception{
-		
-		return controler.getInfoHospedagem(email, atributo);
+	public String getInfoHospedagem(String email, String atributo) throws Exception {
+
+		return controler.getInfo(email, atributo);
 	}
-	
-	public String realizaCheckout(String email, String quarto) throws Exception{
+
+	public String realizaCheckout(String email, String quarto) throws Exception {
 		return controler.realizaCheckout(email, quarto);
 	}
-	
-	public String consultaTransacoes(String atributo){
+
+	public String consultaTransacoes(String atributo) {
 		return controler.consultaTransacoes(atributo);
 	}
 
-	public String consultaTransacoes(String atributo, int indice) throws Exception{
+	public String consultaTransacoes(String atributo, int indice) throws Exception {
 		return controler.consultaTransacoes(atributo, indice);
 	}
+
 	
+	public String consultaRestaurante(String nome, String atributo) throws Exception {
+		return controlerRestaurante.consultaRestaurante(nome, atributo);
+	}
+
+	public void cadastraRefeicao(String nome, String descricao, String componentes) throws Exception {
+		controlerRestaurante.cadastraRefeicao(nome, descricao, componentes);
+	}
+
+	public RefeicaoCompleta buscaRefeicao(String nome) {
+		return controlerRestaurante.buscaRefeicao(nome);
+	}
+
+	public Prato buscaCardapio(String nome) {
+		return controlerRestaurante.buscaCardapio(nome);
+	}
+	public boolean removeCardapio(Prato prato) {
+		return controlerRestaurante.removeCardapio(prato);
+	}
+	public boolean cadastraPrato(String nome,  double preco ,String descricao) throws Exception {
+		return controlerRestaurante.cadastraPrato(nome, preco, descricao);
+	}
+	public double compraPrato(Prato prato) throws Exception {
+		return controlerRestaurante.compraPrato(prato);
+	}
 	
 	public void fechaSistema() {
 		controler.fechaSistema();
 	}
-
 	public static void main(String[] args) {
-		args = new String[] {"hotel.Facade", "acceptance_test/teste_uc1", "acceptance_test/testes_uc1_exception"};
+		args = new String[] { "sistema.Facade", "testes/testes_uc4_exception.txt" };
 		EasyAccept.main(args);
 	}
+
 }
