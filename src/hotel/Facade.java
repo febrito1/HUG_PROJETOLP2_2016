c39976ddfc6daea9e3c3a94fe60ce9ce823de661
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2016-09-21 12:07:08 -0300

    Nova Facade

--- src/hotel/Facade.java
+++ src/hotel/Facade.java
@@ -8,88 +8,89 @@ import restaurante.RestauranteController;
 
 public class Facade {
 
-	private SistemaController controler;
-	private RestauranteController controlerRestaurante;
+	private SistemaController controller;
 
 	public Facade() {
-		controler = new SistemaController();
-		controlerRestaurante =  new RestauranteController();
+		controller = new SistemaController();
 
 	}
 
 	public void iniciaSistema() {
-		controler.iniciaSistema();
+		controller.iniciaSistema();
 	}
 
 	public String cadastraHospede(String nome, String email, String dataNascimento) throws Exception {
-		return controler.cadastraHospede(nome, email, dataNascimento);
+		return controller.cadastraHospede(nome, email, dataNascimento);
 	}
 
 	public String getInfoHospede(String id, String atributo) throws Exception {
-		return controler.getInfo(id, atributo);
+		return controller.getInfo(id, atributo);
 	}
 
 	public void atualizaCadastro(String id, String atributo, String valor) throws Exception {
-		controler.atualizaCadastro(id, atributo, valor);
+		controller.atualizaCadastro(id, atributo, valor);
 
 	}
 
 	public void removeHospede(String email) throws Exception {
-		controler.removeHospede(email);
+		controller.removeHospede(email);
 	}
 
 	public void realizaCheckin(String email, int dias, String idQuarto, String tipoQuarto) throws Exception {
-		controler.realizaCheckin(email, dias, idQuarto, tipoQuarto);
+		controller.realizaCheckin(email, dias, idQuarto, tipoQuarto);
 	}
 
 	public String getInfoHospedagem(String email, String atributo) throws Exception {
 
-		return controler.getInfo(email, atributo);
+		return controller.getInfo(email, atributo);
 	}
 
 	public String realizaCheckout(String email, String quarto) throws Exception {
-		return controler.realizaCheckout(email, quarto);
+		return controller.realizaCheckout(email, quarto);
 	}
 
 	public String consultaTransacoes(String atributo) {
-		return controler.consultaTransacoes(atributo);
+		return controller.consultaTransacoes(atributo);
 	}
 
 	public String consultaTransacoes(String atributo, int indice) throws Exception {
-		return controler.consultaTransacoes(atributo, indice);
+		return controller.consultaTransacoes(atributo, indice);
 	}
 
-	
 	public String consultaRestaurante(String nome, String atributo) throws Exception {
-		return controlerRestaurante.consultaRestaurante(nome, atributo);
+		return controller.consultaRestaurante(nome, atributo);
 	}
 
 	public void cadastraRefeicao(String nome, String descricao, String componentes) throws Exception {
-		controlerRestaurante.cadastraRefeicao(nome, descricao, componentes);
+		controller.cadastraRefeicao(nome, descricao, componentes);
 	}
 
 	public RefeicaoCompleta buscaRefeicao(String nome) {
-		return controlerRestaurante.buscaRefeicao(nome);
+		return controller.buscaRefeicao(nome);
 	}
 
 	public Prato buscaCardapio(String nome) {
-		return controlerRestaurante.buscaCardapio(nome);
+		return controller.buscaCardapio(nome);
 	}
+
 	public boolean removeCardapio(Prato prato) {
-		return controlerRestaurante.removeCardapio(prato);
+		return controller.removeCardapio(prato);
 	}
-	public boolean cadastraPrato(String nome,  double preco ,String descricao) throws Exception {
-		return controlerRestaurante.cadastraPrato(nome, preco, descricao);
+
+	public boolean cadastraPrato(String nome, double preco, String descricao) throws Exception {
+		return controller.cadastraPrato(nome, preco, descricao);
 	}
+
 	public double compraPrato(Prato prato) throws Exception {
-		return controlerRestaurante.compraPrato(prato);
+		return controller.compraPrato(prato);
 	}
-	
+
 	public void fechaSistema() {
-		controler.fechaSistema();
+		controller.fechaSistema();
 	}
+
 	public static void main(String[] args) {
-		args = new String[] { "sistema.Facade", "testes/testes_uc4_exception.txt" };
+		args = new String[] { "sistema.Facade", "testes/testes_uc3.txt" };
 		EasyAccept.main(args);
 	}
 
